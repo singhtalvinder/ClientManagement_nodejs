a76b367f368d149b4598a77c8c82ae446640f242
singhtalvinder <talvindersingh.ca@gmail.com>
2019-06-02 15:39:54 -0400

    Modified to use Async Await.

--- src/index.js
+++ src/index.js
@@ -11,42 +11,103 @@ const port = process.env.port || 3000;
 app.use(express.json())
 
 // Create new user.
-app.post('/users', (req, res) => {
+// Async functions return a promise, so handle these gracefully.
+app.post('/users', async (req, res) => {
     console.log(req.body)
 
     // create the new user
     const user = new User(req.body)
+    try {
+        await user.save()
+        // next line of code is executed only if the above promise is 
+        // fullfilled. It will not execute if its rejected.
 
-    user.save().then(() => {
         res.status(201).send(user)
-    }).catch((err) => { 
+    } catch(err) {
         res.status(400).send("An error occured while adding user: " + err)
-    })
+    }
 })
 
 // Get all users.
-app.get('/users', (req, res) => {
-    User.find({
-        // fetch all users.
-    }).then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        await User.find({})
         res.send(users)
-    }).catch((err) => {
+
+    }catch(err) {
         res.status(500).send("Failed to fetch users: " + err)
-    })
+    }
 
 })
 
-// Get a user. id - can be any name.
-app.get('/users/:id', (req, res) => {
+// Get a user by name.
+app.get('/users/:name', async (req, res) => {
     // access all request params.
+    console.log('Params = ' + req.params)
+    console.log(' Params name = ' +req.params.name)
+
+    // Mongoose converts the string ids to object ids and 
+    // we do not need to do that conversion.
+    const findName = req.params.name
+
+    try {
+        // fetch a user.
+        await User.findOne({name: findName })
+        if(!user) {
+            return res.status(404).send("User not found")
+        }
+
+        // Got a user.
+        res.send(user)
+    } catch(err) {
+        res.status(500).send("Error: " +err + ".  Details:Failed to fetch user with name: " + findName)
+    }
+
+})
+
+// // Get a user by name and count of docs.
+// app.get('/users/:name', (req, res) => {
+//     // access all request params.
+//     console.log('Params = ' + req.params)
+//     console.log(' Params name = ' +req.params.name)
+
+//     // Mongoose converts the string ids to object ids and 
+//     // we do not need to do that conversion.
+//     const findName = req.params.name
+
+//     // fetch all users.
+//     User.findOne({name: findName }).then((user) => {
+//         if(!user) {
+//             return res.status(404).send("User not found")
+//         }
+
+//         console.log('Found the user!! ')
+//         return User.countDocuments()
+
+//         // Got a user.
+//         //res.send(user)
+
+//     }).then((docCount) => {
+//         console.log('doc count is : ' + docCount)
+//         res.send(user)
+//     }).catch((err) => {
+//         res.status(500).send("Error: " +err + ".  Details:Failed to fetch user with name: " + findName)
+//     })
+
+// })
+
+// Get a user. id - can be any name.
+app.get('/users/:id', async (req, res) => {
+    // Access all request params.
     console.log(req.params)
 
     // Mongoose converts the string ids to object ids and 
     // we do not need to do that conversion.
     const _id = req.params.id
 
-    // fetch all users.
-    User.findById(_id).then((user) => {
+    try {
+        // Fetch user by id.
+        await User.findById(_id)
         if(!user) {
             return res.status(404).send("User not found")
         }
@@ -54,14 +115,37 @@ app.get('/users/:id', (req, res) => {
         // Got a user.
         res.send(user)
 
-    }).catch((err) => {
-        res.status(500).send("Error: " +err + ".  Details:Failed to fetch user with id: " + _id)
-    })
+    } catch(err)  {
+         res.status(500).send("Error: " +err + ".  Details:Failed to fetch user with id: " + _id)
+    }
 
 })
 
+// Delete a user by name.
+app.post('/users/:name', async (req, res) => {
+    // access all request params.
+    console.log('Params = ' + req.params)
+    console.log(' Params name = ' +req.params.name)
 
+    // Mongoose converts the string ids to object ids and 
+    // we do not need to do that conversion.
+    const findName = req.params.name
 
+    try {
+        // fetch all users.
+        await User.findOneAndDelete({name: findName })
+        if(!user) {
+            return res.status(404).send("User not found")
+        }
+
+        // Got a user.
+        res.send(user)
+    } catch(err) {
+        res.status(500).send("Error: " +err + ".  Details:Failed to delete user with name: " + findName)
+    }
+})
+
+// Run and listen on the port.
 app.listen(port, () =>{
     console.log('Server started at port: ' + port);
 })
